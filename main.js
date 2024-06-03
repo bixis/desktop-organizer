925d3a74119b99d4ff31e8e2443979be1463e905
bixis <julianlinarescalero@gmail.com>
2024-06-02 21:39:43 -0300

    update main menu

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const {app, BrowserWindow} = electron;
+const {app, BrowserWindow, Menu} = electron;
 
 let mainWindow;
 
@@ -14,8 +14,37 @@ app.on('ready', function(){
 	// Load html
 	mainWindow.loadURL(url.format({
 		pathname: path.join(__dirname, "mainWindow.html"),
-		protocol: 'file',
+		protocol: 'file:',
 		slashes: true	
 	}));
-	
+	// Building menu
+	const mainMenu = Menu.buildFromTemplate(mainMenuTemplate)
+	Menu.setApplicationMenu(mainMenu)
 });
+
+
+// Create menu template
+const mainMenuTemplate = [
+	{
+		label: "File",
+		submenu: [
+			{
+				label:"Add Item" // not yet implemeted
+
+			},
+			{
+				label:"Clear Items" // not yet implemented
+			},
+			{
+				label:"Quit",
+				accelerator: process.platform == 'darwin' ? 'Command+Q':'Ctrl+Q',
+				click(){
+					app.quit();
+				}
+			},
+		
+		]
+	}
+]
+
+
